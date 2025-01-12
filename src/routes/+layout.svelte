3ddc0c7dc2a09c0ecab79a3abada7914afdd0bc6
Simon <hello@gamersi.at>
2025-01-12 23:14:59 +0100

    feat: dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import '../app.css';
+	import { ModeWatcher } from "mode-watcher";
 
 	let { children } = $props();
 </script>
 
 <div class="app">
 	<main>
+		<ModeWatcher />
 		{@render children()}
 	</main>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card/index.js';
+	import Sun from "lucide-svelte/icons/sun";
+	import Moon from "lucide-svelte/icons/moon";
+	
+	import { toggleMode } from "mode-watcher";
+	import { Button } from "$lib/components/ui/button/index.js";
 </script>
 
 <svelte:head>
@@ -13,10 +18,19 @@
 			<Card.Title>Welcome to my quizapp</Card.Title>
 		</Card.Header>
 		<Card.Content>
-			<p>
+			<p class="pb-4">
 				Go to a specific route to view or go to <a href="/admin" class="text-blue-500">/admin</a> to
 				create quizes
 			</p>
+			<Button onclick={toggleMode} variant="outline" size="icon">
+				<Sun
+				  class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+				/>
+				<Moon
+				  class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+				/>
+				<span class="sr-only">Toggle theme</span>
+			  </Button>
 		</Card.Content>
 	</Card.Root>
 </section>
